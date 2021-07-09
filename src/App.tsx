1a26807db1fff1f8e911d31bb073e670dcac370b
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2021-07-09 11:50:17 -0300

    Estrutura inicial Front - Prova SERAp

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>
+  );
+}
+
+export default App;
