04ac3658c82fb8566b5668fa6803e5c52a6cb0a3
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2021-07-24 18:42:42 -0300

    add font, padrao atomic, material ui, pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css';
-import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
+import GlobalStyle from './styles/globals';
 
 function App() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-    </header>
+    <>
+      <GlobalStyle />
+      <Button variant="contained">Default</Button>
+    </>
   );
 }
 
---
+++ src/styles/globals.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+import './roboto.css';
+
+export default createGlobalStyle`
+ 
+  *,
+  html,
+  body {
+    font-family: 'Roboto', sans-serif; 
+  }
+`;
