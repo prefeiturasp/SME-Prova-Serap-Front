696250589088c9ee3cc8b9d0a95a36d1f223174e
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2021-07-26 04:39:39 -0300

    add component Button

---
+++ src/components/atoms/button/button.tsx
@@ -0,0 +1,40 @@
+import MuiButton, { ButtonProps } from '@material-ui/core/Button';
+import {
+  createStyles,
+  StyleRules,
+  Theme,
+  WithStyles,
+  withStyles,
+} from '@material-ui/core/styles';
+import React from 'react';
+import { Colors } from '../colors/colors';
+
+const styles: (theme: Theme) => StyleRules<string> = (theme) =>
+  createStyles({
+    root: {
+      fontSize: '16px',
+      color: Colors.White,
+      backgroundColor: Colors.Primary,
+      height: 48,
+      '&:hover': {
+        backgroundColor: Colors.Primary,
+      },
+      borderRadius: '12px',
+    },
+  });
+
+interface OwnProps {
+  text: string;
+}
+
+type PublicProps = OwnProps & ButtonProps;
+type Props = PublicProps & WithStyles<typeof styles>;
+
+const Button: React.FC<Props> = ({ classes, text, ...rest }) => {
+  return (
+    <MuiButton {...rest} className={classes.root}>
+      {text}
+    </MuiButton>
+  );
+};
+export default withStyles(styles)(Button) as React.ComponentType<PublicProps>;
