d6ac73b10cbb1862b25385c7b2966c850db8c8c6
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2021-07-29 20:14:01 -0300

    add redux no app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
 import { createTheme, Grid, MuiThemeProvider } from '@material-ui/core';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Redirect, Route, Router, Switch } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import GlobalStyle from './components/atoms/styles/globals';
 import Login from './components/pages/login/login.page';
+import { persistor, store } from './redux';
+import history from './services/history';
 
 const theme = createTheme({
   palette: {
@@ -19,28 +23,32 @@ function App() {
   const logado = false;
 
   return (
-    <MuiThemeProvider theme={theme}>
-      <Grid
-        container
-        alignItems="center"
-        justifyContent="center"
-        style={{ minHeight: '100vh' }}
-      >
-        <Grid item xl={6}>
-          <BrowserRouter>
-            <GlobalStyle />
-            <Switch>
-              <Route path="/login" component={Login} />
-              {logado ? (
-                <Route component={() => <div>home</div>} path="/" />
-              ) : (
-                <Redirect to="/login" />
-              )}
-            </Switch>
-          </BrowserRouter>
-        </Grid>
-      </Grid>
-    </MuiThemeProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router history={history}>
+          <MuiThemeProvider theme={theme}>
+            <Grid
+              container
+              alignItems="center"
+              justifyContent="center"
+              style={{ minHeight: '100vh' }}
+            >
+              <Grid item xl={6}>
+                <GlobalStyle />
+                <Switch>
+                  <Route path="/login" component={Login} />
+                  {logado ? (
+                    <Route component={() => <div>home</div>} path="/" />
+                  ) : (
+                    <Redirect to="/login" />
+                  )}
+                </Switch>
+              </Grid>
+            </Grid>
+          </MuiThemeProvider>
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
