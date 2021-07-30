92dfe288b29d5df22f0a4430b829ee3a9b7caafb
Cleiton Dias Dutra <cleitondutra2203@gmail.com>
2021-07-30 18:35:38 -0300

    novas cores

--- src/components/atoms/styles/colors.js
+++ src/components/atoms/styles/colors.js
@@ -2,6 +2,9 @@ const colors = {
   primary: '#FF7755',
   inputBackground: '#f3f3f3',
   white: '#FFFFFF',
+  nav: '#2F304E',
+  actionNav: '#F2945B',
+  backgroundGeral: '#fff8f3',
 };
 
 export default colors;
